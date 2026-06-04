b1cb12974e0918cd5d89be2eccccec6936498bc2
Anatoliy_Sysoev <anatoliisysoev@gmail.com>
2026-06-04 17:23:19 +0300

    Wire FTT stage deliverables search route

--- src/asu_june_bot/search/__init__.py
+++ src/asu_june_bot/search/__init__.py
@@ -2,5 +2,8 @@
 
 from .models import SearchRequest, SearchResponse
 from .service import SearchService
+from .ftt_stage_route import patch_search_service
+
+patch_search_service(SearchService)
 
 __all__ = ["SearchRequest", "SearchResponse", "SearchService"]
